7f1394c94266929d25de0c6957c960cc2be4ca60
gabe thomas <gabe.thomas@bizzabo.com>
2021-04-17 12:33:35 -0400

    waiting data

---
+++ data/waitinglistData.js
@@ -0,0 +1,12 @@
+// const waiting = [
+//     {
+//         routeName: "bendurham",
+//         name: "Ben Durham",
+//         tableNum: 1,
+//         email: "ben@test.com",
+//         phone: 1234567890
+
+//     },
+// ];
+
+// module.exports = waiting;
