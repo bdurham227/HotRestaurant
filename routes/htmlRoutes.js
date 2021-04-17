c0f87e35995e749aac11132e1a4ddfaf4a926a95
bdurham227 <bdurham227@gmail.com>
2021-04-17 12:17:43 -0400

    added __ to dirname and added (app) to require on app.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,13 +8,13 @@ module.exports = (app) => {
     // res.send("Welcome to the Star Wars Page!")
     // app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
     app.get('/view', (req, res) => {
-        res.sendFile(path.join(_dirname, '../public/view.html'));
+        res.sendFile(path.join(__dirname, '../public/view.html'));
     });
     app.get('/add', (req, res) => {
-        res.sendFile(path.join(_dirname, '../public/add.html'));
+        res.sendFile(path.join(__dirname, '../public/add.html'));
     });
     //one more for no table found?
     app.get('/', (req, res) => {
-        res.sendFile(path.join(_dirname, '../public/home.html'));
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 }
